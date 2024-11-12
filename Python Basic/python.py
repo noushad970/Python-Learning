917b46632d823e2931b2f1d5bd7c015b65f0e6bb
noushad970 <ug2102038@cse.pstu.ac.bd>
2024-11-12 10:28:16 +0600

    learning python basic

--- Python Basic/python.py
+++ Python Basic/python.py
@@ -1,1 +1,30 @@
+''' hello '''
 print("Hello")
+sk="My name is noushad"
+print(sk)
+tt= sk+" bhuiyan"
+print(tt)
+x=10
+print(type(x))
+x=10j
+print(x)
+isActive=True
+print(isActive)
+#byte and byte array
+b=[1,2,3,4,3,5,6,7]
+by=bytes(b)
+print(by[0])
+byteA=bytearray(b)
+print(byteA[0])
+byteA[0]=100
+print(byteA[0])
+#list
+lst=[1,3,45,6,7]
+print(lst)
+print(lst[0])
+lst.insert(3,10)
+print(lst)
+lst.append(199)
+print(lst)
+lst.append("NewList")
+print(lst)
