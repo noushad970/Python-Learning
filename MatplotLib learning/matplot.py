2b80dcb16262f5c2ebe56326039c328f725c2bda
noushad970 <ug2102038@cse.pstu.ac.bd>
2025-07-11 03:57:02 +0600

    learning pandas and matplotlib

---
+++ MatplotLib learning/matplot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4]
+y=[12,31,43,12]
+plt.plot(x,y)
+plt.show()
--- NumPy Learning/numpy_test.py
+++ NumPy Learning/numpy_test.py
@@ -1,3 +1,7 @@
 import numpy as py
+import pandas as p
 numpy_array=py.array([[1,2,3],[4,6,7],[1,3,43]])
 print(numpy_array)
+pandaData=p.DataFrame(numpy_array)
+print("->")
+print(pandaData)
